4b714ead144f20c42ac694f69e6a1244c26bbebd
Ivan Diordiev <i.diordiev@outlook.com>
2021-12-11 18:19:37 +0200

    added user page

--- OnlineStore/Controllers/AccountController.cs
+++ OnlineStore/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -19,6 +20,65 @@ namespace OnlineStore.Controllers
         {
             _userManager = userManager;
             _signInManager = signInManager;
+        }
+
+
+        public IActionResult Index()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                string id = _userManager.GetUserId(HttpContext.User);
+
+                User user = _userManager.FindByIdAsync(id).Result;
+
+                UserPageViewModel model = new UserPageViewModel()
+                {
+                    Id = user.Id,
+                    Username = user.UserName,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    PhoneNumber = user.PhoneNumber,
+                    City = user.City,
+                    Address = user.Address
+                };
+                return View(model);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UserPageViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    user.Id = model.Id;
+                    user.UserName = model.Username;
+                    user.FirstName = model.FirstName;
+                    user.LastName = model.LastName;
+                    user.Email = model.Email;
+                    user.PhoneNumber = model.PhoneNumber;
+                    user.City = model.City;
+                    user.Address = model.Address;
+                    
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(String.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return RedirectToAction("Index", model);
         }
 
         [HttpGet]
